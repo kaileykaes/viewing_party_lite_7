c2c4e69e02fc8d970f72748ac6a26cdedb7a7b86
Nick Tassinari <ntass7800@gmail.com>
2023-07-07 14:47:50 -0600

    add vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,11 @@ RSpec.configure do |config|
     end
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<MOVIE_DB_KEY>') {ENV["MOVIE_DB_KEY"]}
+  config.allow_http_connections_when_no_cassette = true
+
+end
