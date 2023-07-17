d53a4dda0e46ea3276099d93377ece12dfc362dc
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-17 12:37:23 -0600

    add additional test for password security

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe User, type: :model do
     it { should validate_confirmation_of(:password) }
     it { should have_secure_password }
   end
+
+  describe 'security' do 
+    it 'doesnt store password as attribute' do 
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
+  end
 end
