72cd971f250650375d2b940df7e7d0101ead1620
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-17 12:33:07 -0600

    add password validations tests to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -10,5 +8,8 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of(:password_digest)}
+    it { should validate_confirmation_of(:password) }
+    it { should have_secure_password }
   end
 end
