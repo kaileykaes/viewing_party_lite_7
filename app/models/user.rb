c1bc2ad461731d8c0e6d94b6ce5bd2bfaa6757dd
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-17 13:29:12 -0600

    fix test for user password validation, add password validations and confirmations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :viewing_party_users
   has_many :viewing_parties, through: :viewing_party_users
   validates :email, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  has_secure_password
 end
