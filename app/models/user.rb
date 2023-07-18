2af73ddb291e011e4daf32f8017bcf8f3afe16c6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-18 12:05:26 -0600

    add user validations for attribute presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_many :viewing_party_users
   has_many :viewing_parties, through: :viewing_party_users
+
   validates :email, uniqueness: true
+
+  validates :name, presence: true, confirmation: true
+  validates :email, presence: true, confirmation: true
   validates :password, presence: true, confirmation: true
+  
   has_secure_password
 end
