5849be7f07a9ff81960ab1313e9ccb3cef6aa94b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-18 16:38:19 -0600

    test: login happy path

--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -44,19 +44,3 @@ RSpec.describe 'landing page' do
     end
   end
 end
-
-# User Story #3 - Logging In Happy Path
-# As a registered user
-# When I visit the landing page `/`
-# I see a link for "Log In"
-# When I click on "Log In"
-# I'm taken to a Log In page ('/login') where I can input my unique email and password.
-# When I enter my unique email and correct password 
-# I'm taken to my dashboard page
-
-# Notes for User Story #3:
-# You will need to create two routes for this user story (one for going to a /login page, and one for actually checking credentials and directing traffic). These routes DO NOT need to be ReSTful right now. We'll talk about how to make them ReSTful tomorrow. For now, you might consider doing something like this:
-# GET '/login', to: 'users#login_form'
-# login_form will render login_form.html.erb for users to fill in a form with their credentials
-# POST '/login', to: 'users#login_user'
-# login_user will check if a user exists with the email address that was provided, then check to see if the password, when hashed, matches the secure password stored in the database, and then redirects the user based on if credentials are correct.
