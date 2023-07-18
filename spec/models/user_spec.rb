f78b85f77d991c96a3c92518694ac60c75b8b5cd
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-18 12:04:04 -0600

    test attribute validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe User, type: :model do
   end
 
   describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of(:password)}
+    it { should validate_presence_of :password}
     it { should validate_confirmation_of(:password) }
     it { should have_secure_password }
   end
