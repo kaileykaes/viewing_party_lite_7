77f23870d67cc9c9c6a1de1917159bbfc725cdb4
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-23 15:31:05 -0600

    refactor tests to log user in

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe 'User Show Page' do
   describe 'user dash' do
     before(:each) do
-      @user1 = User.create!(name: 'Kevdog', email: 'BigKev@gmail.com', password: 'pass112')
+      @user_1 = User.create!(name: 'Kevdog', email: 'BigKev@gmail.com', password: 'pass112')
       @party1 = ViewingParty.create!(duration: 120, date: '05-03-2024', time: '03:00:00', movie_id: 1)
-      @viewing_party_user1 = ViewingPartyUser.create!(user_id: @user1.id, viewing_party_id: @party1.id)
+      @viewing_party_user1 = ViewingPartyUser.create!(user_id: @user_1.id, viewing_party_id: @party1.id)
 
-      visit "/users/#{@user1.id}"
+      visit login_path
+      fill_in :email, with: @user_1.email
+      fill_in :password, with: @user_1.password
+      click_button 'Log In'
+      visit user_path(@user_1)
     end
 
     it "has user's name Dash at top of page" do
-      expect(page).to have_content("#{@user1.name}'s Dashboard")
+      expect(page).to have_content("#{@user_1.name}'s Dashboard")
     end
 
     it 'has Button to Discover Movies' do
